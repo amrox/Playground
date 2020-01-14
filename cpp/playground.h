ae488d1f786d4d7363563d92f2adc31788a1a20c
Andy Mroczkowski <a@mrox.co>
2020-01-13 21:17:36 -0500

    c++ updates

--- cpp/playground.h
+++ cpp/playground.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#include <optional>
-#include <vector>
 #include <algorithm>
+#include <numeric>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <vector>
 
 namespace playground {
 
@@ -31,4 +34,93 @@ namespace playground {
 
         return h.front();
     }
+
+    // straightforward, imperative
+    template<typename T>
+    auto sum_div_by_3_or_5(T const up_to) -> long long
+    {
+        static_assert(std::is_integral<T>::value, "Integral required.");
+
+        long long out(0);
+
+        for (T i = 3; i <= up_to; i += 3) {
+            out += i;
+        }
+
+        for (T i = 5; i <= up_to; i += 5) {
+            out += i;
+        }
+        return out;
+    }
+
+    // sorta functional approach using stl 
+    template<typename T>
+    auto sum_div_by_3_or_5_stl(T const up_to) -> long long
+    {
+        static_assert(std::is_integral<T>::value, "Integral required.");
+
+        // generate all numbers up to limit
+        std::vector<T> all_up_to(up_to);
+        std::generate(all_up_to.begin(), all_up_to.end(), [n = 1] () mutable { return n++; });
+
+        // filter list 
+        std::vector<T> filtered_up_to;
+        std::copy_if(all_up_to.begin(), all_up_to.end(),
+                     std::back_inserter(filtered_up_to),
+                     [&](const auto &elem) {
+                         return elem % 3 == 0 || elem % 5 == 0;
+                     });
+
+        // sum
+        return std::accumulate(filtered_up_to.begin(), filtered_up_to.end(), 0);
+    }
+
+    template <typename T>
+    auto gcd(T a, T b) -> T
+    {
+        static_assert(std::is_integral<T>::value, "Integral required.");
+        
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    class IPV4 {
+
+    public:
+        IPV4(uint8_t o1, uint8_t o2, uint8_t o3, uint8_t o4) : _ip{o1, o2, o3, o4}
+        {
+        }
+
+        static std::optional<IPV4> from_string(std::string const & str)
+        {
+            return std::nullopt;
+        }
+
+        IPV4() = delete;
+
+        IPV4(IPV4 const & other) = default;
+
+        IPV4 &operator=(IPV4 const & other) = default;
+
+        auto to_string() -> std::string
+        {
+            // TODO: fmt would be nice here
+            std::ostringstream ss;
+            ss << std::to_string(_ip[0]) << ".";
+            ss << std::to_string(_ip[1]) << ".";
+            ss << std::to_string(_ip[2]) << ".";
+            ss << std::to_string(_ip[3]);
+            return ss.str();
+        }
+
+    private:
+        uint8_t _ip[4];
+    };
 }
+
--- cpp/tests/test_playground.cpp
+++ cpp/tests/test_playground.cpp
@@ -26,3 +26,46 @@ TEST_CASE( "Find the kth smallest value in a list", "[]" ) {
     CHECK( playground::kth(std::vector{ 9, 6, 12, 3 }, 3).value() == 9 );
     CHECK( playground::kth(std::vector{ 9, 6, 12, 3 }, 4).value() == 12 );
 }
+
+// The Modern C++ Challenge 1
+TEST_CASE("Write a program that calculates and prints the sum of all the natural numbers divisible by either 3 or 5, up to a given limit entered by the user.")
+{
+    CHECK( playground::sum_div_by_3_or_5(3) == 3 );
+    CHECK( playground::sum_div_by_3_or_5(5) == 8 );
+    CHECK( playground::sum_div_by_3_or_5(10) == 33 );
+    
+    CHECK( playground::sum_div_by_3_or_5_stl(3) == 3 );
+    CHECK( playground::sum_div_by_3_or_5_stl(5) == 8 );
+    CHECK( playground::sum_div_by_3_or_5_stl(10) == 33 );
+}
+
+
+// The Modern C++ Challenge 2
+TEST_CASE("Write a program that, given two positive integers, will calculate and print the greatest common divisor of the two.")
+{
+    CHECK( playground::gcd(105, 50) == 5 );
+}
+
+// The Modern C++ Challenge 14
+TEST_CASE("Write a program that validates that 10-digit values entered by the user, as a string, represent valid ISBN-10 numbers.")
+{
+    // TODO:
+}
+
+// Write a class that represents an IPv4 address. Implement the functions
+// required to be able to read and write such addresses from or to the console.
+// The user should be able to input values in dotted form, such as 127.0.0.1 or
+// 168.192.0.100. This is also the form in which IPv4 addresses should be
+// formatted to an output stream.
+TEST_CASE("IPv4 data type")
+{
+    CHECK((playground::IPV4(127, 0, 0, 1)).to_string() == "127.0.0.1");
+
+    playground::IPV4 ip1 { 192, 168, 10, 5 };
+    playground::IPV4 ip2 { ip1 };
+    CHECK(ip2.to_string() == "192.168.10.5");
+
+    CHECK(playground::IPV4::from_string("pizza!") == std::nullopt);
+    
+    // TODO: parse the ip address
+}
