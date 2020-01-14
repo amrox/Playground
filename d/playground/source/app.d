196996b4ffc5e4c47cf9e0c6a595c3195cd986e0
Andy Mroczkowski <a@mrox.co>
2020-01-13 21:16:53 -0500

    D stuff

---
+++ d/playground/source/app.d
@@ -0,0 +1,20 @@
+import std.stdio;
+
+void main()
+{
+	writeln("Edit source/app.d to start your project.");
+}
+
+// sum 2 ints
+int sum(int x, int y) {
+	return x + y;
+}
+
+int kth(int[]) {
+
+}
+
+unittest {
+	assert(sum(2, 2) == 4);
+}
+
